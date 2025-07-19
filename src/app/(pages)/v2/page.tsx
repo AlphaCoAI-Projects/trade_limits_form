c9f1ac05c25edbd1f5178d7915a09ae44a050b48
iPranav-Patil <spydycool@gmail.com>
2025-07-19 21:04:23 +0530

    :hammer: Added editable table from tanstack react table

--- src/app/(pages)/v2/page.tsx
+++ src/app/(pages)/v2/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FormTable } from "./_components/table";
+import FormHeader from "./_components/header";
+import FormContent from "./_components/content";
+
 
 const TradeFormEditableTable = () => {
   return (
-    <div>TradeFormEditableTable</div>
-  )
-}
+    <div className="h-auto md:m-10 m-4 flex flex-col justify-center items-center">
+      <div>
+        <FormHeader />
+      </div>
+      <div>
+        <FormTable />
+      </div>
+      <div>
+        <FormContent />
+      </div>
+    </div>
+  );
+};
 
-export default TradeFormEditableTable
+export default TradeFormEditableTable;
